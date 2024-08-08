65e385c8abab7cff772087c483ba138a1a572243
vincentvnoord <vannoord.vincent@gmail.com>
2024-08-08 14:00:20 +0200

    Dynamic height issues

--- src/components/AuthLayoutContext.tsx
+++ src/components/AuthLayoutContext.tsx
@@ -16,7 +16,7 @@ export default function AuthLayoutContext({ children, menu }: { children: React.
                         {menu}
                     </Navbar>
                     <MainContent>
-                        <div className="flex w-full flex-col gap-4 md:p-6">
+                        <div className="flex w-full h-full flex-col gap-4 md:p-6">
                             {children}
                         </div>
                     </MainContent>
@@ -31,12 +31,12 @@ export default function AuthLayoutContext({ children, menu }: { children: React.
 }
 
 const Navbar = ({ children, className }: { children?: React.ReactNode, className?: string }) => {
-    const defaultStyle = "absolute pointer-events-none z-30 w-full h-full xl:p-6 xl:pl-0 bottom-0 flex flex-col items-center xl:max-w-[400px] xl:sticky xl:max-h-screen";
+    const defaultStyle = "absolute pointer-events-none z-30 w-full h-full xl:p-6 xl:pl-0 bottom-0 flex flex-col items-center xl:max-w-[400px] xl:sticky xl:max-h-dvh";
 
     return (
         <div className={`${defaultStyle} ${className}`}>
             <div className={`absolute transition-all duration-200 ease-in z-10 inset-0`}></div>
-            <div className="sticky z-30 top-0 max-h-screen flex flex-col items-center w-full h-full xl:p-0">
+            <div className="sticky z-30 top-0 max-h-dvh flex flex-col items-center w-full h-full xl:p-0">
                 <div className="w-full h-full xl:h-fit justify-end items-center flex flex-col gap-1 overflow-hidden xl:max-w-[400px]">
                     {children}
                 </div>
